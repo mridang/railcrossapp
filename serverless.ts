a394ff6f562b2f7f7d750993d9033ffac99d0c9d
Mridang Agarwalla <mridang@nosto.com>
2024-02-12 18:48:15 +0700

    Rectified the IAM permissions to allow creating schedules

--- serverless.ts
+++ serverless.ts
@@ -30,7 +30,7 @@ const serverlessConfiguration: AWS = {
                   'arn:aws:scheduler',
                   { Ref: 'AWS::Region' },
                   { Ref: 'AWS::AccountId' },
-                  'schedule/railcross-lock-unlock-schedules',
+                  'schedule/railcross-lock-unlock-schedules/*',
                 ],
               ],
             },
