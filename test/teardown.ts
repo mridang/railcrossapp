51127c141364cf539981550b78430292500b6137
Mridang Agarwalla <mridang@nosto.com>
2024-02-18 13:11:38 +0700

    Added support for automatically starting testcontainers

---
+++ test/teardown.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+
+export default async function teardown(): Promise<void> {
+    if (globalThis.DOCKER) {
+        console.info('Stopping docker compose');
+        await globalThis.DOCKER.down({
+            removeVolumes: true,
+        });
+    }
+}
