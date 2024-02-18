e862914a1e1f064160f7d5f48a6fe14d9ebda74e
Mridang Agarwalla <mridang@nosto.com>
2024-02-18 13:41:12 +0700

    Removed the unnecessary .build directory

--- .build/src/constants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.secretName = exports.roleName = exports.scheduleGroup = void 0;
-exports.scheduleGroup = 'railcross-lock-unlock-schedules';
-exports.roleName = 'RailcrossSchedulerRole';
-exports.secretName = 'LockdownAppConfig';
-//# sourceMappingURL=constants.js.map
