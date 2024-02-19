92ee798fd2a16d9d165684d473efb27a2542c6e9
Mridang Agarwalla <mridang@nosto.com>
2024-02-19 20:08:20 +0700

    Added a health-check controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
 
-@Controller('status')
+@Controller('health')
 export class AppController {
-  @Get('/')
-  async getHello(): Promise<string> {
-    return 'ok';
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {
+    //
+  }
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('1.1.1.1', 'https://1.1.1.1/'),
+    ]);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { RailcrossModule } from './services/railcross/railcross.module';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
     RailcrossModule,
+    TerminusModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
