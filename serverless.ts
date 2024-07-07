56c517c6f6884576de0783791a621c49cdc1d891
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-07-07 11:37:59 +0530

    Added the resource-tag plugin to better group resources

--- serverless.ts
+++ serverless.ts
@@ -14,6 +14,7 @@ const serverlessConfiguration: AWS = {
     'serverless-plugin-typescript',
     '@mridang/serverless-checkov-plugin',
     '@mridang/serverless-shortsha-plugin',
+    '@mridang/serverless-resourcetag-plugin',
   ],
   package: {
     individually: false,
