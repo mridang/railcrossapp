75ae1a4fe74a92bc0f1fa8ca86090dc9f5e69f9d
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-10-07 10:47:53 +0700

    Bumped the @mridang/nestjs-defaults package to get the latest version of Sentry

--- knip.config.js
+++ knip.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  entry: ['src/main.ts', 'src/event.ts'],
+  entry: ['src/main.ts', 'src/event.ts', 'serverless.ts'],
   ignore: ['public/js/tailwind.3.4.5.js'],
+  ignoreDependencies: [/.*serverless-.*/],
 };
